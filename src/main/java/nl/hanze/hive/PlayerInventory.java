0259bd6651d7450a12c48fb7d27b78ff19205bf2
Mees van Dijk <mees@mees.io>
2022-11-04 20:51:36 +0100

    Improve playerinventory

--- src/main/java/nl/hanze/hive/PlayerInventory.java
+++ src/main/java/nl/hanze/hive/PlayerInventory.java
@@ -1,22 +1,27 @@
 package nl.hanze.hive;
 
-import nl.hanze.hive.pieces.*;
-
 import java.util.HashMap;
 
 public class PlayerInventory {
-//Elke speler heeft aan het begin van het spel de beschikking over één
-//bijenkoningin, twee spinnen, twee kevers, drie soldatenmieren en drie
-//sprinkhanen in zijn eigen kleur.
+    // Elke speler heeft aan het begin van het spel de beschikking over één
+    // bijenkoningin, twee spinnen, twee kevers, drie soldatenmieren en drie
+    // sprinkhanen in zijn eigen kleur.
 
-    HashMap<Enum,Integer> PlayerInventories = new HashMap<Enum, Integer>();
+    HashMap<Hive.Tile, Integer> PlayerInventories = new HashMap<Hive.Tile, Integer>();
 
-    public void setPlayerInventories(HashMap<Enum, Integer> playerInventories) {
-        PlayerInventories = playerInventories;
-        playerInventories.put(Hive.Tile.QUEEN_BEE,1);
-        playerInventories.put(Hive.Tile.SPIDER,2);
-        playerInventories.put(Hive.Tile.BEETLE,2);
-        playerInventories.put(Hive.Tile.SOLDIER_ANT,3);
-        playerInventories.put(Hive.Tile.GRASSHOPPER,3);
+    public PlayerInventory(Hive.Player player) {
+        PlayerInventories.put(Hive.Tile.QUEEN_BEE, 1);
+        PlayerInventories.put(Hive.Tile.SPIDER, 2);
+        PlayerInventories.put(Hive.Tile.BEETLE, 2);
+        PlayerInventories.put(Hive.Tile.SOLDIER_ANT, 3);
+        PlayerInventories.put(Hive.Tile.GRASSHOPPER, 3);
+    }
+
+    public void removePiece(Hive.Tile tile) {
+        PlayerInventories.put(tile, PlayerInventories.get(tile) - 1);
+    }
+
+    public boolean canPlayPiece(Hive.Tile tile) {
+        return PlayerInventories.get(tile) > 0;
     }
 }

858d56582deff19b1880ef18e8fb379ad76051e4
Mees van Dijk <mees@mees.io>
2022-11-07 13:32:34 +0100

    Add more of a game

--- src/test/java/nl/hanze/hive/HiveImplTest.java
+++ src/test/java/nl/hanze/hive/HiveImplTest.java
@@ -1,7 +1,8 @@
 package nl.hanze.hive;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +22,7 @@ public class HiveImplTest {
     assertFalse(hive.isWinner(Player.WHITE));
 
     hive.play(Tile.BEETLE, 0, -1);
-    hive.play(Tile.BEETLE, 0, 2);
+    hive.play(Tile.BEETLE, 1, 1);
 
     assertFalse(hive.isDraw());
     assertFalse(hive.isWinner(Player.BLACK));
@@ -34,10 +35,54 @@ public class HiveImplTest {
     assertFalse(hive.isWinner(Player.BLACK));
     assertFalse(hive.isWinner(Player.WHITE));
 
-    hive.move(1, -1, 1, 0);
+    hive.play(Tile.SPIDER, -1, 0);
     hive.move(-1, 2, -1, 1);
 
-    // TODO: implement this test further
-    fail("Not yet implemented");
+    assertFalse(hive.isDraw());
+    assertFalse(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+
+    hive.play(Tile.SPIDER, 0, -2);
+    hive.move(1, 1, 1, 0);
+
+    assertFalse(hive.isDraw());
+    assertTrue(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+  }
+
+  @Test()
+  public void playAGame() throws IllegalMove {
+    HiveImpl hive = new HiveImpl();
+    hive.play(Tile.QUEEN_BEE, 0, 0);
+    hive.play(Tile.QUEEN_BEE, 0, 1);
+
+    assertFalse(hive.isDraw());
+    assertFalse(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+
+    hive.play(Tile.BEETLE, 0, -1);
+    hive.play(Tile.BEETLE, 1, 1);
+
+    assertFalse(hive.isDraw());
+    assertFalse(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+
+    hive.play(Tile.BEETLE, 1, -1);
+    hive.play(Tile.BEETLE, -1, 2);
+
+    assertFalse(hive.isDraw());
+    assertFalse(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+
+    hive.play(Tile.SPIDER, -1, 0);
+    hive.move(-1, 2, -1, 1);
+
+    assertFalse(hive.isDraw());
+    assertFalse(hive.isWinner(Player.BLACK));
+    assertFalse(hive.isWinner(Player.WHITE));
+
+    hive.move(-1, 0, 1, -2);
+    hive.move(1, 1, 0, 2);
+
   }
 }

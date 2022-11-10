c7e02cc0b88ea60621f1efd56785adf4a58a04e3
Mees van Dijk <mees@mees.io>
2022-11-10 15:27:18 +0100

    Improve path tests

--- src/test/java/nl/hanze/hive/PathTest.java
+++ src/test/java/nl/hanze/hive/PathTest.java
@@ -1,14 +1,12 @@
 package nl.hanze.hive;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
-
-import nl.hanze.hive.Path.Step;
 
 public class PathTest {
   @Test()
@@ -16,48 +14,43 @@ public class PathTest {
     ArrayList<Position> adjacentPositions = new ArrayList<Position>();
     adjacentPositions.add(new Position(0, -1));
     adjacentPositions.add(new Position(1, -1));
-    adjacentPositions.add(new Position(1, 0));
-    adjacentPositions.add(new Position(0, 1));
-    adjacentPositions.add(new Position(-1, 1));
 
-    new Path(adjacentPositions);
+    assertDoesNotThrow(() -> new Path(adjacentPositions));
   }
 
   @Test()
   public void nonAdjacentPositions() {
     ArrayList<Position> adjacentPositions = new ArrayList<Position>();
     adjacentPositions.add(new Position(0, -1));
-    adjacentPositions.add(new Position(1, -1));
-    adjacentPositions.add(new Position(1, 0));
     adjacentPositions.add(new Position(0, 1));
-    adjacentPositions.add(new Position(-1, 3));
 
-    assertThrows(IllegalArgumentException.class, () -> {
-      new Path(adjacentPositions);
-    });
+    assertThrows(IllegalArgumentException.class, () -> new Path(adjacentPositions));
   }
 
   @Test()
-  public void matchingPathStartEnd() {
+  public void matchingPathStart() {
     ArrayList<Position> adjacentPositions = new ArrayList<Position>();
     adjacentPositions.add(new Position(0, -1));
     adjacentPositions.add(new Position(1, -1));
-    adjacentPositions.add(new Position(1, 0));
-    adjacentPositions.add(new Position(0, 1));
-    adjacentPositions.add(new Position(-1, 1));
 
     Path path = new Path(adjacentPositions);
 
     assertEquals(path.from, new Position(0, -1));
-    assertEquals(path.to, new Position(-1, 1));
-
-    assertEquals(path.from, path.steps.get(0).from);
-    assertEquals(path.to, path.steps.get(path.steps.size() - 1).to);
-    assertEquals(path.steps.size(), 4);
   }
 
   @Test()
-  public void matchingStepStartEnd() {
+  public void matchingPathEnd() {
+    ArrayList<Position> adjacentPositions = new ArrayList<Position>();
+    adjacentPositions.add(new Position(-1, 0));
+    adjacentPositions.add(new Position(-1, 1));
+
+    Path path = new Path(adjacentPositions);
+
+    assertEquals(path.to, new Position(-1, 1));
+  }
+
+  @Test()
+  public void pathDistance() {
     ArrayList<Position> adjacentPositions = new ArrayList<Position>();
     adjacentPositions.add(new Position(0, -1));
     adjacentPositions.add(new Position(1, -1));
@@ -66,13 +59,8 @@ public class PathTest {
     adjacentPositions.add(new Position(-1, 1));
 
     Path path = new Path(adjacentPositions);
-    Iterator<Step> iter = path.steps.iterator();
-    Step previous = iter.next();
-    while (iter.hasNext()) {
-      Step current = iter.next();
-      assertEquals(previous.to, current.from);
-      previous = current;
-    }
+
+    assertEquals(path.steps.size(), 4);
   }
 
 }

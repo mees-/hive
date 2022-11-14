0b2295aca17052ab74a3e3b81faad788328c81e2
Mees van Dijk <mees@mees.io>
2022-11-14 20:32:51 +0100

    Add test for grasshopper move

---
+++ src/test/java/nl/hanze/hive/pieces/GrasshopperTest.java
@@ -0,0 +1,36 @@
+package nl.hanze.hive.pieces;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nl.hanze.hive.mocks.BoardMock;
+import nl.hanze.hive.Position;
+import nl.hanze.hive.Hive.Player;
+
+public class GrasshopperTest {
+
+  private BoardMock standardBoard;
+
+  @BeforeEach()
+  public void setUp() {
+    standardBoard = new BoardMock();
+
+    standardBoard.putPiece(new QueenBee(Player.WHITE), new Position(0, 0));
+    standardBoard.putPiece(new QueenBee(Player.BLACK), new Position(0, 1));
+    standardBoard.putPiece(new Beetle(Player.WHITE), new Position(1, -1));
+    standardBoard.putPiece(new Beetle(Player.BLACK), new Position(1, 1));
+    standardBoard.putPiece(new Spider(Player.WHITE), new Position(2, -1));
+    standardBoard.putPiece(new Grasshopper(Player.BLACK), new Position(3, -1));
+  }
+
+  // REQ: 11a
+  @Test()
+  public void validMove() {
+    Piece piece = standardBoard.getPiece(new Position(3, -1));
+    assertTrue(piece.isValidMove(standardBoard, new Position(3, -1), new Position(0, -1)));
+  }
+
+}

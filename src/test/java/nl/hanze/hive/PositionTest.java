de2b7a4aa32443f054c1e6d49901c921b5896b3e
Mees van Dijk <mees@mees.io>
2022-11-14 15:42:59 +0100

    Add test for distance with corner to left

--- src/test/java/nl/hanze/hive/PositionTest.java
+++ src/test/java/nl/hanze/hive/PositionTest.java
@@ -10,7 +10,7 @@ public class PositionTest {
     Position a = new Position(1, 2);
     Position b = new Position(2, 1);
 
-    assertEquals(1, a.distanceTo(b));
+    assertEquals(1, Position.distanceBetween(a, b));
   }
 
   @Test()
@@ -22,10 +22,18 @@ public class PositionTest {
   }
 
   @Test()
-  public void distanceWithCorner() {
+  public void distanceWithCornerToRight() {
     Position a = new Position(-1, 0);
     Position b = new Position(3, -2);
 
     assertEquals(4, Position.distanceBetween(a, b));
+  }
+
+  @Test()
+  public void distanceWithCornerToLeft() {
+    Position a = new Position(0, 0);
+    Position b = new Position(1, -3);
+
+    assertEquals(3, Position.distanceBetween(a, b));
   }
 }

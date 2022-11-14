acbaa8e09f890db3fd933ec46b554a27ef915d7b
Mees van Dijk <mees@mees.io>
2022-11-14 20:49:41 +0100

    Pass test

--- src/main/java/nl/hanze/hive/pieces/Grasshopper.java
+++ src/main/java/nl/hanze/hive/pieces/Grasshopper.java
@@ -12,6 +12,6 @@ public class Grasshopper extends Piece {
 
     @Override
     public boolean isValidMove(Board board, Position from, Position to) {
-        return true;
+        return Position.isStraightLine(from, to);
     }
 }
